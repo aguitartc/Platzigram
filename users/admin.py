cf088515b1c6b15ab50ddfc0ee3f5a1dd3f286a6
ana <ana.guitart@gmail.com>
2023-06-04 11:33:46 +0200

    FUNCIONA corritgeix assignació atribute model

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,9 @@
+#django
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
+
+#models
+from django.contrib.auth.models import User
 from users.models import Profile
 
 # Register your models here.
@@ -13,6 +18,7 @@ class ProfileAdmin(admin.ModelAdmin):
     list_editable = ('phone_number','picture')
     search_fields = ('phone_number','picture')
     list_filter = ('user__is_active','user__is_staff')
+    readonly_fields = ('created','modified')
     fieldsets = (
         ('Profile', {
             'fields': (
@@ -21,6 +27,26 @@ class ProfileAdmin(admin.ModelAdmin):
         }),
         ('Extra Info', {
             'fields': (
-                ('website', 'biography')),
-        })
+                ('website', 'phone_number'),
+                ('biography')
+                ),
+        }),
+        ('Metadata',{
+            'fields': (
+                ('created','modified'),
+            )})
     )
+
+class ProfileInline(admin.StackedInline):
+    """Profile inline admin for users"""
+    model = Profile
+    can_delete = False 
+    verbose_name_plural = 'profiles'
+
+class UserAdmin(BaseUserAdmin):
+    """Add profile admin to base user admin"""    
+
+    inlines = (ProfileInline,)
+
+admin.site.unregister(User)
+admin.site.register(User,UserAdmin)
