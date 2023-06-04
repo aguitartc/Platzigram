f796f8735345e53a623f84403aa8cb6c1672c861
ana <ana.guitart@gmail.com>
2023-06-04 12:11:57 +0200

    Afegeix Profile i nous camps a la creació/edició de user/admin

--- users/admin.py
+++ users/admin.py
@@ -47,6 +47,8 @@ class UserAdmin(BaseUserAdmin):
     """Add profile admin to base user admin"""    
 
     inlines = (ProfileInline,)
+    list_display = ('username','email','first_name',
+                    'last_name','is_active','is_staff')
 
 admin.site.unregister(User)
 admin.site.register(User,UserAdmin)
