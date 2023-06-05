74b29925b1adac9b7d46c86177bf43817f47c5e5
ana <ana.guitart@gmail.com>
2023-06-05 19:20:40 +0200

    Afegeix posts a admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,1 +1,34 @@
 from django.contrib import admin
+
+#models
+from posts.models import Post
+
+# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Posts admin."""
+
+    list_display = ('user' , 'title', 'photo', 'created', 'modified', 'profile')
+    #editant aquest atribut, fem que en clicar user i phone_number ens porti al detaLL del user
+    list_display_links = ('user',)
+
+    list_editable = ('title', 'photo', 'profile')
+    search_fields = ('user__username', 'user__email', 'title')
+    
+    readonly_fields = ('created','modified')
+    fieldsets = (
+        ('Profile', {
+            'fields': (
+                ('user'), 'profile',
+                ),
+        }),
+        ('Info post', {
+            'fields': (
+                ('title', 'photo'),
+                ),
+        }),
+        ('Metadata',{
+            'fields': (
+                ('created','modified'),
+            )})
+    )
--- users/models.py
+++ users/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-#from django.contrib.auth.models import admin
 from django.contrib.auth.models import User
 
 #Models
-#from users.models import Profile
 # Create your models here.
 
 class Profile(models.Model):
@@ -15,7 +13,7 @@ class Profile(models.Model):
      created = models.DateTimeField(auto_now_add=True)
      modified = models.DateTimeField(auto_now=True)
 
-#     def __str__(self):
-#         """return username"""
-#         return self.user.username
+     def __str__(self):
+         """return username"""
+         return self.user.username
 
