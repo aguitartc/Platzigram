d2f28369a910348a7d0f217e623af3e4aa2e8181
ana <ana.guitart@gmail.com>
2023-06-25 11:05:04 +0200

    arregla path users posant users/

--- Platzigram/urls.py
+++ Platzigram/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     
     path('', include(('posts.urls','posts'),namespace='posts')),
 
-    path('', include(('users.urls','users'),namespace='users'))
+    path('users/', include(('users.urls','users'),namespace='users'))
         
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
