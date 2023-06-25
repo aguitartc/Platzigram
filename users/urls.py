bf95c75b6af0dbcd436e3299007d05be848079c2
ana <ana.guitart@gmail.com>
2023-06-25 12:00:54 +0200

    es mou la url detail del user al final pq funcioni el logout i login , no els pilli com usernames

--- users/urls.py
+++ users/urls.py
@@ -6,12 +6,7 @@ from django.views.generic import TemplateView
 from users import views 
 
 urlpatterns = [
-    #Posts
-    path(
-        route = '<str:username>/',
-        view=views.UserDetailView.as_view(),
-        name='detail'
-    ),
+    
     #Management
     path(
         route='login/',
@@ -30,12 +25,17 @@ urlpatterns = [
         view=views.signup,
         name='signup'
     ),
-    
+
     path(
         route='me/profile/',
         view=views.update_profile,
         name='update_profile'
     ),
 
-    
+    #Posts
+    path(
+        route = '<str:username>/',
+        view=views.UserDetailView.as_view(),
+        name='detail'
+    ),
 ]
